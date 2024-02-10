1e844742068d625ec0d9bed5a0207dea8f0f1e12
GadzZzilo <GadzZzilo@yandex.ru>
2024-02-10 19:46:34 +0300

    новый функционал

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from bot.handlers.common_handlers import register_common_handlers
 from bot.handlers.cutter_fsm import register_cutter_handlers
 from bot.handlers.cutter_drawing import register_cutter_drawing_handlers
 from bot.handlers.cancel_handlers import register_cancel_handlers
+from bot.handlers.broach_drawing import register_broach_drawing_handlers
 
 
 def register_handler(dp: Dispatcher) -> None:
@@ -16,6 +17,7 @@ def register_handler(dp: Dispatcher) -> None:
     register_cutter_handlers(dp)
     register_broach_handlers(dp)
     register_cutter_drawing_handlers(dp)
+    register_broach_drawing_handlers(dp)
     register_common_handlers(dp)
 
 
